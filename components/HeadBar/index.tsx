536210afe02ed380817155b0c5b1c4cb424666b3
GweepCreative <gweepcreative@gmail.com>
2025-03-09 18:33:16 +0300

    Greetings

--- components/HeadBar/index.tsx
+++ components/HeadBar/index.tsx
@@ -1,15 +1,27 @@
 import { View, Image } from 'react-native'
 import { Text } from '../ui'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 export default function HeadBar() {
+    const greeting = useMemo(() => {
+        const currentHour = new Date().getHours();
+        
+        if (currentHour >= 5 && currentHour < 10) {
+            return "günaydın";
+        } else if (currentHour >= 10 && currentHour < 18) {
+            return "iyi günler";
+        } else {
+            return "iyi akşamlar";
+        }
+    }, []);
+
     return (
         <View className='px-6 py-4 flex flex-row items-center gap-2 border-b border-zinc-100'>
             <View>
                 <Image source={require('@/assets/images/icon.png')} className='w-16 h-16' />
             </View>
             <View className='flex flex-col'>
-                <Text>Merhaba, iyi günler</Text>
+                <Text>Merhaba, {greeting}</Text>
                 <View className='flex flex-row items-center gap-2'>
                     <Text variant='xl' className='font-bold'>Gürkan</Text>
                     <Text variant='xl' className='font-bold'>👋</Text>
