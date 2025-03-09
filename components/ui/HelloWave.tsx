3f44579a40bff919161c43f3d8b5332f2664fead
GweepCreative <gweepcreative@gmail.com>
2025-03-09 22:00:31 +0300

    Always Wave

--- components/ui/HelloWave.tsx
+++ components/ui/HelloWave.tsx
@@ -1,26 +1,36 @@
 import { useEffect } from "react";
 import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSequence, withTiming } from "react-native-reanimated";
 import Text from "./Text";
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 
 export function HelloWave() {
     const rotationAnimation = useSharedValue(0);
 
+
     useEffect(() => {
+        animation();
+    }, []);
+
+    const animation = () => {
         rotationAnimation.value = withRepeat(
             withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
             4 // Run the animation 4 times
         );
-    }, []);
-
+    }
     const animatedStyle = useAnimatedStyle(() => ({
         transform: [{ rotate: `${rotationAnimation.value}deg` }],
     }));
 
     return (
-        <Animated.View style={animatedStyle}>
-            <Text variant="xl">👋</Text>
-        </Animated.View>
+        <Pressable
+            onPress={() => {
+                animation();
+            }}
+        >
+            <Animated.View style={animatedStyle} >
+                <Text variant="xl">👋</Text>
+            </Animated.View>
+        </Pressable>
     );
 }
 const styles = StyleSheet.create({
